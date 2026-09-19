6ca495c79e7107ad5db5826404ef74752e774dda
sue1231511 <sue1231511@gmail.com>
2026-09-19 14:32:16 +0800

    消息中心：注册 ensure_pages 里的消息页

--- koilink-theme/functions.php
+++ koilink-theme/functions.php
@@ -49,8 +49,9 @@ add_action( 'wp_enqueue_scripts', function () {
  */
 function koilink_ensure_pages() {
 	$pages = array(
-		'publish' => array( '发布', 'template-publish.php' ),
-		'me'      => array( '我的', 'template-me.php' ),
+		'publish'  => array( '发布', 'template-publish.php' ),
+		'me'       => array( '我的', 'template-me.php' ),
+		'messages' => array( '消息', 'template-messages.php' ),
 	);
 	foreach ( $pages as $slug => $conf ) {
 		if ( ! get_page_by_path( $slug ) ) {
